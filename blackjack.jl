b0a66bc8d2dd10212134533b1390ec9fab3dfb8d
Jacob Caesar <jacobcaesar@Jacobs-MacBook-Pro.local>
2022-04-23 16:04:33 -0700

    .... something is happening

--- blackjack.jl
+++ blackjack.jl
@@ -1,52 +1,70 @@
 using QuickPOMDPs: QuickMDP
 using POMDPs
-using POMDPModelTools: Deterministic, Uniform
+using POMDPModelTools: Deterministic, Uniform, SparseCat
 using POMDPPolicies: FunctionPolicy
 using POMDPSimulators: RolloutSimulator
 
 
-
+cards = [:ace, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10] # 5 10s for the face cards + 10
+# state[1] = players hand 
+total_idx = 1
+# state[2] = dealer showing 
+dealer_showing_idx = 2
+# state[3] = usable ace?
+usable_ace_idx = 3
 bj = QuickMDP(
- 
-    # state[1] = players hand 
-    # state[2] = dealer showing 
-    # TODO: state[3] = usable ace? 
     actions = [:hit,:stay], # could add double down or surrender
-    transition = function (s, a)
-        
-        if s[1] == -1 # turn is already over 
-            splayer = Deterministic(-1)
+    function (s, a, rng)
+        sp_out = [-1, -1, false] # terminal state
+        r_out = 0.0
+        if s[total_idx] > 21 # went over 21, player automatically loses
+            r_out = -1.0
+        elseif s[total_idx] == 21 # player hit 21, player automatically wins? 
+            r_out = 1.0
+        elseif a == :stay 
+            # no change in state need to add dealer behavior
 
-        elseif s[1] >= 21 # player has gone over 21, turn ends
-            splayer =  Deterministic(-1)
-             
-        elseif a == :hit # hitting randomly selects a card
-            splayer =  Uniform(s[1] .+ [1,2,3,4,5,6,7,8,9,10])
-        elseif a == :stay
-            splayer =  Deterministic(-1) # staying ends turn 
+            # dealer behavior then reward then terminal state
+        elseif a ==:hit 
+            new_card = rand(cards)
+            if new_card == :ace 
+                sp_out[total_idx] = s[total_idx] + 11 # 2 usable aces???
+                sp_out[usable_ace_idx] = true
+            else
+                sp_out[total_idx] = s[total_idx] + new_card
+            end
+               
+        elseif s[total_idx] > 21 # player has gone over 21
+            
+            if s[usable_ace_idx] == :usable_ace && (s[total_idx] - 11 + 1) <=  21 # if you have a useable ace, use it
+                sp_out = s
+                sp_out[usable_ace_idx] = false # used up the ace
+                sp_out[total_idx] = s[total_idx] - 11 + 1 # ace turns into a 1, update the count
+            else 
+                sp_out = s # can't do anything about it, will lose next turn
+            end
         end
-        sout = (splayer, Deterministic(s[2]))
-        return sout
+        return (sp=sp_out, r=r_out)
     end,
 
-    reward = function (s, a)
-        if s[1] > 21 # player has gone over and loses
-            return -1.0
-        elseif s[1] == 21 # player has hit 21 and won
-            return 1.0
-        elseif s[1] < 21 # game is still going 
-            return 0.0
-        end
-    end,
+    # reward = function (s, a)
+    #     if s[1] > 21 # player has gone over and loses
+    #         return -1.0
+    #     elseif s[1] == 21 # player has hit 21 and won
+    #         return 1.0
+    #     elseif s[1] < 21 # game is still going 
+    #         return 0.0
+    #     end
+    # end,
     observation = function (s, a, sp)
         return s # not a pomdp, the exact state is known
     end,
 
     # This doesn't work 
     # initialstate = (Uniform([1,2,3,4,5,6,7,8,9,10]), Deterministic(0)), # intial draw from the deck 
-    initialstate = Deterministic((0, 0)), # intial draw from the deck 
+    initialstate = [0, 10, false], # intial draw from the deck 
     discount = 1.0, # not a discounted game 
-    isterminal = function (s)
+    isterminal = function(s)
         if s[1] == -1 # -1 is our terminal state
             return true
         else 
