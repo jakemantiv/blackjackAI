ddc73bf97a9109a2b6f425337e7930defa8749ed
Jacob Caesar <jacobcaesar@Jacobs-MacBook-Pro.local>
2022-04-23 20:11:07 -0700

    added dealer behavior... something is wrong with this one:

--- blackjack.jl
+++ blackjack.jl
@@ -5,26 +5,66 @@ using POMDPPolicies: FunctionPolicy
 using POMDPSimulators: RolloutSimulator
 
 
-cards = [:ace, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10] # 5 10s for the face cards + 10
-# state[1] = players hand 
+# helper functions
+cards = [:ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] # 4 10s for the face cards + 10
+# state[1] = players hand
 total_idx = 1
-# state[2] = dealer showing 
+# state[2] = dealer showing
 dealer_showing_idx = 2
 # state[3] = usable ace?
 usable_ace_idx = 3
+
+get_initial_state = function()
+    player_draw = rand(cards)
+    dealer_draw = rand(cards)
+    if player_draw == :ace
+        player_total = 11
+        usable_ace = true
+    else 
+        player_total = player_draw
+        usable_ace = false
+    end
+    return (player_total, dealer_draw, usable_ace)
+end
+
 bj = QuickMDP(
     actions = [:hit,:stay], # could add double down or surrender
-    function (s, a, rng)
-        sp_out = [-1, -1, false] # terminal state
+    function(s, a, rng)
+        println(s)
+        sp_out = (-1, -1, false) # terminal state
         r_out = 0.0
         if s[total_idx] > 21 # went over 21, player automatically loses
             r_out = -1.0
-        elseif s[total_idx] == 21 # player hit 21, player automatically wins? 
+        elseif s[total_idx] == 21 # player hit 21, player automatically wins?
             r_out = 1.0
-        elseif a == :stay 
-            # no change in state need to add dealer behavior
+        elseif a == :stay
+            # no change in player count, dealer follows a set strategy
+            dealer_turn_over = false
+            first_ace = s[dealer_showing_idx] != :ace # was the dealer already showing an ace?
+            while !dealer_turn_over
+                new_dealer_card = rand(cards)
+                if new_dealer_card == :ace
+                    first_ace = false
+                    if first_ace # first ace counts as 11, subsequent aces count as 1
+                        dealer_total += 11
+                    else
+                        dealer_total += 1  
+                    end
+                    first_ace = false
+                end
+                if dealer_total >= 17
+                    dealer_turn_over = true
+                end
+            end
 
-            # dealer behavior then reward then terminal state
+            if dealer_total == s[total_idx]
+                r_out = 0.0 # a tie!
+            elseif dealer_total > s[total_idx]
+                r_out = -1.0 # a loss!
+            else # dealer_total < s[total_idx]
+                r_out = 1.0 # a win!
+            end
+            
         elseif a ==:hit 
             new_card = rand(cards)
             if new_card == :ace 
@@ -41,29 +81,20 @@ bj = QuickMDP(
                 sp_out[usable_ace_idx] = false # used up the ace
                 sp_out[total_idx] = s[total_idx] - 11 + 1 # ace turns into a 1, update the count
             else 
-                sp_out = s # can't do anything about it, will lose next turn
+                sp_out = s # no useable aces to save you, will lose next turn
             end
         end
         return (sp=sp_out, r=r_out)
     end,
 
-    # reward = function (s, a)
-    #     if s[1] > 21 # player has gone over and loses
-    #         return -1.0
-    #     elseif s[1] == 21 # player has hit 21 and won
-    #         return 1.0
-    #     elseif s[1] < 21 # game is still going 
-    #         return 0.0
-    #     end
+    # observation = function (s, a, sp)
+    #     return s # not a pomdp, the exact state is known
     # end,
-    observation = function (s, a, sp)
-        return s # not a pomdp, the exact state is known
-    end,
 
-    # This doesn't work 
-    # initialstate = (Uniform([1,2,3,4,5,6,7,8,9,10]), Deterministic(0)), # intial draw from the deck 
-    initialstate = [0, 10, false], # intial draw from the deck 
-    discount = 1.0, # not a discounted game 
+    # initialstate = get_initial_state(), # intial draw from the deck
+    initialstate = (2,5,false), # intial draw from the deck - test
+
+    discount = 1.0, # not a discounted game
     isterminal = function(s)
         if s[1] == -1 # -1 is our terminal state
             return true
@@ -73,7 +104,7 @@ bj = QuickMDP(
     end
 )
 
-policy = FunctionPolicy(a->POMDPs.actions(bj)[1]) # always hit policy 
+policy = FunctionPolicy(a->POMDPs.actions(bj)[1]) # evaluate always hit policy
 sim = RolloutSimulator(max_steps=100)
 @show reward_ = [POMDPs.simulate(sim, bj, policy) for _ in 1:100]
 @show mean(reward_)
