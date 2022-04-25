bc9469ef23bf714cecc467749adc473f377143f2
Jacob Caesar <jacobcaesar@Jacobs-MacBook-Pro.local>
2022-04-24 18:47:28 -0700

    reworked some dealer behavior and player behavior surrounding useable ace

--- blackjack.jl
+++ blackjack.jl
@@ -1,6 +1,6 @@
 using QuickPOMDPs: QuickMDP
 using POMDPs
-using POMDPModelTools: Deterministic, Uniform, SparseCat
+using POMDPModelTools: Deterministic, Uniform, SparseCat, ImplicitDistribution
 using POMDPPolicies: FunctionPolicy
 using POMDPSimulators: RolloutSimulator
 
@@ -28,6 +28,7 @@ get_initial_state = function()
 end
 
 function my_expert_policy(s)
+    #  https://wizardofodds.com/games/blackjack/strategy/4-decks/
     player_total_in = s[total_idx]
     dealer_showing_in = s[dealer_showing_idx]
     useable_ace_in = s[useable_ace_idx]
@@ -67,6 +68,58 @@ function my_expert_policy(s)
     return action_out
 end
 
+function simulate_dealer(dealer_showing_in)
+    dealer_turn_over = false
+    dealer_useable_ace = dealer_showing_in != :ace # was the dealer already showing an ace?
+    dealer_blackjack = false
+    num_turns = 1
+
+    if dealer_showing_in == :ace
+        dealer_total = 11
+    else
+        dealer_total = dealer_showing_in
+    end
+
+    while !dealer_turn_over
+        num_turns +=1 # keep count of draws for natural blackjack
+        new_dealer_card = rand(cards) # draw a new card
+        if new_dealer_card == :ace 
+            if num_turns == 2 && (dealer_total + 11) == 21
+                dealer_blackjack = true 
+                dealer_total += 11 # natural blackjack
+            elseif dealer_useable_ace == true && (dealer_total + 11) > 21
+                dealer_total += 1 + 1 -11
+                dealer_useable_ace = false 
+            elseif dealer_useable_ace == true && (dealer_total + 11) <= 21
+                dealer_total += 11
+                dealer_useable_ace = true 
+            elseif dealer_useable_ace == false && (dealer_total + 11) <= 21
+                dealer_total += 11
+                dealer_useable_ace = true 
+            elseif dealer_useable_ace == false && (dealer_total + 11) > 21 
+                dealer_total += 1
+            else
+                println(dealer_showing_in) 
+            end
+
+        else
+            if num_turns == 2 && (dealer_total + new_dealer_card) == 21
+                dealer_blackjack = true 
+                dealer_total += new_dealer_card # natural blackjack
+            elseif dealer_useable_ace == true && (dealer_total + new_dealer_card) > 21
+                dealer_total += new_dealer_card - 11 + 1
+            else
+                dealer_total += new_dealer_card 
+            end
+        end
+        if dealer_total >= 17 || dealer_blackjack # stand on soft 17
+            dealer_turn_over = true
+        end
+    end
+
+    return (dealer_blackjack, dealer_total)
+end
+
 bj = QuickMDP(
     actions = [:hit,:stay], # could add double down or surrender
     function(s, a, rng)
@@ -82,37 +135,17 @@ bj = QuickMDP(
         if s[total_idx] > 21 # went over 21, player automatically loses
             r_out = -1.0
             sp_out = (player_total_out, dealer_showing_out, useable_ace_out)
-        elseif s[total_idx] == 21 # player hit 21, player automatically wins?
-            r_out = 1.0
+        elseif s[total_idx] == 21 # player hit 21, player automatically wins unless dealer blackjacks? 
+            dealer_blackjack, dealer_total = simulate_dealer(dealer_showing_in)
+            if dealer_blackjack
+                r_out = 0.0
+            else
+                r_out = 1.0
+            end
             sp_out = (player_total_out, dealer_showing_out, useable_ace_out)
         elseif a == :stay
             # no change in player count, dealer follows a set strategy
-            dealer_turn_over = false
-            first_ace = dealer_showing_in != :ace # was the dealer already showing an ace?
-
-            if dealer_showing_in == :ace
-                dealer_total = 11
-            else
-                dealer_total = dealer_showing_in
-            end
-
-            while !dealer_turn_over
-                new_dealer_card = rand(cards)
-                if new_dealer_card == :ace
-                    first_ace = false
-                    if first_ace # first ace counts as 11, subsequent aces count as 1
-                        dealer_total += 11
-                    else
-                        dealer_total += 1  
-                    end
-                    first_ace = false
-                else
-                    dealer_total += new_dealer_card 
-                end
-                if dealer_total >= 17
-                    dealer_turn_over = true
-                end
-            end
+            dealer_blackjack, dealer_total = simulate_dealer(dealer_showing_in)
 
             if dealer_total == player_total_in
                 r_out = 0.0 # a tie!
@@ -127,24 +160,43 @@ bj = QuickMDP(
 
         elseif a ==:hit 
             new_card = rand(cards)
-            if new_card == :ace 
-                player_total_out = player_total_in + 11  # 2 useable aces???
-                useable_ace_out = true
-                dealer_showing_out = dealer_showing_in
-            else
-                player_total_out = player_total_in + new_card
-                useable_ace_out = useable_ace_in
-                dealer_showing_out = dealer_showing_in
-            end
-            sp_out = (player_total_out, dealer_showing_out, useable_ace_out)
 
-        elseif s[total_idx] > 21 # player has gone over 21
-            
-            if s[useable_ace_idx] == true && (s[total_idx] - 11 + 1) <=  21 # if you have a useable ace, use it
-                player_total_out = player_total_in - 11 + 1 # ace turns into a 1, update the count
-                dealer_showing_out = dealer_showing_in
-                useable_ace_out = false # used up the ace
-            else 
+            if useable_ace_in == true 
+                if new_card == :ace && ((player_total_in + 1) <= 21)
+                    player_total_out = player_total_in + 1
+                    dealer_showing_out = dealer_showing_in
+                    useable_ace_out = true
+                    # accept the ace as an 1 value, still maintain the useable ace
+                elseif new_card == :ace && ((player_total_in + 1) > 21)
+                    player_total_out = player_total_in - 11 + 1
+                    dealer_showing_out = dealer_showing_in
+                    useable_ace_out = false
+                    # accept the ace as an 1 value, use the useable ace
+                elseif new_card != :ace && ((player_total_in + new_card) > 21)
+                    player_total_out = player_total_in - 11 + 1 + new_card
+                    dealer_showing_out = dealer_showing_in
+                    useable_ace_out = false
+                else # new_card != :ace
+                    player_total_out = player_total_in + new_card
+                    dealer_showing_out = dealer_showing_in
+                    useable_ace_out = useable_ace_in
+                end
+            else
+                if new_card == :ace && ((player_total_in + 11) <= 21) 
+                    player_total_out = player_total_in + 11
+                    dealer_showing_out = dealer_showing_in
+                    useable_ace_out = true
+                    # accept the ace as an 11 value, now have a useable ace
+                elseif new_card == :ace && ((player_total_in + 11) > 21)
+                    player_total_out = player_total_in + 11
+                    dealer_showing_out = dealer_showing_in
+                    useable_ace_out = false
+                    # accepting the ace as an 11 would put us over, use the ace as a 1, still no usable ace 
+                else #new_card != :ace
+                    player_total_out = player_total_in + new_card
+                    dealer_showing_out = dealer_showing_in
+                    useable_ace_out = false
+                end
             end
             sp_out = (player_total_out, dealer_showing_out, useable_ace_out)
 
@@ -154,12 +206,22 @@ bj = QuickMDP(
 
     # initialstate = get_initial_state(), # intial draw from the deck
     initialstate = Deterministic((5,5,false)), # intial draw from the deck - test
-    # initialstate = Uniform(([2,3,4,5,6,7,8,9,10], [2,3,4,5,], [true,false])),
+    # initialstate = ImplicitDistribution() do rng
+    #             player_card = rand(cards)
+    #             dealer_card = rand(cards)
+    #             if player_card == :ace
+    #                 player_total = 11
+    #             else
+    #                 player_total = player_card
+    #             end
+    #             return (player_total, dealer_card, player_card==:ace)
+    #         end,
+
     discount = 1.0, # not a discounted game
     isterminal = function(s)
         if s[1] == -1 # -1 is our terminal state
             return true
-        else 
+        else
             return false
         end
     end
@@ -167,5 +229,5 @@ bj = QuickMDP(
 # policy = FunctionPolicy(a->actions(bj)[1]) # evaluate always hit policy
 policy = FunctionPolicy(my_expert_policy) # evaluate 'expert' policy 
 sim = RolloutSimulator(max_steps=100)
-@show reward_ = [POMDPs.simulate(sim, bj, policy) for _ in 1:1000]
+@show reward_ = [POMDPs.simulate(sim, bj, policy) for _ in 1:10000]
 @show mean(reward_)
