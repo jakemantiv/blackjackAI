680b4e161f554b1b51d27a71c115d9e5a5289d3f
Jacob Caesar <jacobcaesar@Jacobs-MacBook-Pro.local>
2022-04-25 20:52:09 -0700

    added double q learning

--- blackjack_Qlearning.jl
+++ blackjack_Qlearning.jl
@@ -1,6 +1,8 @@
 include("blackjack.jl")
 using CommonRLInterface: render, actions, act!, observe, reset!, AbstractEnv, observations, terminated, clone
-
+using CommonRLInterface
+using Statistics: mean
+using Plots
 
 
 function double_Q_episode!(Q, Q1, Q2, env; eps=0.10, gamma=0.99, alpha=0.1)
@@ -14,10 +16,10 @@ function double_Q_episode!(Q, Q1, Q2, env; eps=0.10, gamma=0.99, alpha=0.1)
         end
     end
 
-    s = observe(env)
+    s = env.s
     a = policy(s)
     r = act!(env, a)
-    sp = observe(env)
+    sp = env.s
     hist = [s]
 
     while !terminated(env)
@@ -32,14 +34,14 @@ function double_Q_episode!(Q, Q1, Q2, env; eps=0.10, gamma=0.99, alpha=0.1)
         s = sp
         a = ap
         r = act!(env, a)
-        sp = observe(env)
+        sp = env.s
         push!(hist, sp)
     end
 
     Q1[(s,a)] += alpha*(r - Q1[(s, a)])
     Q2[(s,a)] += alpha*(r - Q2[(s, a)])
 
-    for s in observations(env), a in actions(env)
+    for s in states(env.m), a in actions(env)
         Q[(s,a)] = (Q1[(s,a)] + Q2[(s,a)])/2.0 # return average of Q1 and Q2
     end
 
@@ -47,9 +49,9 @@ function double_Q_episode!(Q, Q1, Q2, env; eps=0.10, gamma=0.99, alpha=0.1)
 end
 
 function double_Q!(env; n_episodes=100)
-    Q = Dict((s,a) => 0.0 for s in observations(env), a in actions(env))
-    Q1 = Dict((s,a) => 0.0 for s in observations(env), a in actions(env))
-    Q2 = Dict((s,a) => 0.0 for s in observations(env), a in actions(env))
+    Q = Dict((s,a) => 0.0 for s in states(env.m), a in actions(env))
+    Q1 = Dict((s,a) => 0.0 for s in states(env.m), a in actions(env))
+    Q2 = Dict((s,a) => 0.0 for s in states(env.m), a in actions(env))
     episodes = []
     
     for i in 1:n_episodes
@@ -62,15 +64,67 @@ function double_Q!(env; n_episodes=100)
     return episodes
 end
 
-# env = convert(AbstractEnv, bj)
 
-# N = 500_000
-# # N = 500_000
-# double_Q_episodes = double_Q!(env, n_episodes=N);
+function CommonRLInterface.reset!(m)
+    m.s = (5,5,false)
+end
+
+# function CommonRLInterface.observe(m::MDPCommonRLEnv)
+#     return m.s
+# end
+
+
+env = convert(AbstractEnv, bj)
+
+# N = 5
+N = 5_000
+double_Q_episodes = double_Q!(env, n_episodes=N);
 # lambda_episodes = sarsa_lambda!(env, n_episodes=N);
 
 policy = FunctionPolicy(a->actions(bj)[1]) # evaluate always hit policy
 policy = FunctionPolicy(my_expert_policy) # evaluate 'expert' policy 
 sim = RolloutSimulator(max_steps=100)
 @show reward_ = [POMDPs.simulate(sim, bj, policy) for _ in 1:10000]
-@show mean(reward_)
+@show expert_polcy_mean_reward = mean(reward_)
+
+
+
+
+function my_evaluate(env, policy, n_episodes=1000, max_steps=1000, γ=1.0)
+    returns = Float64[]
+    for _ in 1:n_episodes
+        t = 0
+        r = 0.0
+        reset!(env)
+        s = env.s
+        while !terminated(env)
+            a = policy(s)
+            r += γ^t*act!(env, a)
+            s = env.s
+            t += 1
+        end
+        push!(returns, r)
+    end
+    return returns
+end
+
+
+plotlyjs()
+episodes = Dict("Double-Q"=>double_Q_episodes)
+p = plot(xlabel="steps in environment", ylabel="avg return")
+n = Int(round(N/250))
+stop = N
+for (name, eps) in episodes
+    Q = Dict((s, a) => 0.0 for s in states(env.m), a in actions(env))
+    xs = [0]
+    ys = [mean(my_evaluate(env, s->argmax(a->Q[(s, a)], actions(env))))]
+    for i in n:n:min(stop, length(eps))
+        newsteps = sum(length(ep.hist) for ep in eps[i-n+1:i])
+        push!(xs, last(xs) + newsteps)
+        Q = eps[i].Q
+        push!(ys, mean(my_evaluate(env, s->argmax(a->Q[(s, a)], actions(env)))))
+    end    
+    plot!(p, xs, ys, label=name, legend=:bottomright, minorticks=true)
+end
+plot!(p,[xlims(p)[1], xlims(p)[2]], ones(length(xlims(p))).*expert_polcy_mean_reward,label="Expert Policy")
+display(p)
